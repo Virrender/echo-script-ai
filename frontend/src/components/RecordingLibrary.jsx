1b5aa77b78b1ac91ae4e949183ef88ed18d9d6bf
Virender Kumar <xvirenverse@gmail.com>
2026-07-30 13:27:31 +0530

    Apply pagination with library and do little styling

--- frontend/src/components/RecordingLibrary.jsx
+++ frontend/src/components/RecordingLibrary.jsx
@@ -14,25 +14,13 @@ const [libraryRecordings, setLibraryRecordings] = useState([]);
 const totalPages = Math.ceil(total / limit);
 // const arr=Array.from({length:totalPages}).map((_,index)=>index+1)
 
-const currentPage = page;
-
-let leftPage = currentPage - 1;
-let rightPage = currentPage + 1;
-
-if (leftPage < 1) {
-  leftPage = 1;
-}
-
-if (rightPage > totalPages) {
-  rightPage = totalPages;
-}
 
 
 async function loadLibraryRecordings(page,limit) {
     const token = localStorage.getItem("token");
 
     const response = await fetch(
-     ` http://127.0.0.1:8000/recordings?page=${page}&limit=${limit}`,
+     `http://127.0.0.1:8000/recordings?page=${page}&limit=${limit}`,
 
       {
         headers: {
@@ -73,13 +61,77 @@ async function loadLibraryRecordings(page,limit) {
 //   setPage(1)
 // }
 
+console.log(libraryRecordings);
+    return (
 
-    return (<>
+      
+   <div className="
+    flex
+    flex-col
+    flex-1
+    h-full
+    px-8
+    py-6
+    bg-[#f3e6f4]
+  ">
+    <div   className="
+    flex
+    flex-col
+    items-center
+    justify-center
+    text-center
+    mb-3
+  ">
+
+    <h1
+        className="
+        text-3xl
+        font-bold
+        tracking-tight
+        text-[#2F2A44]
+        "
+    >
+        All Recordings
+    </h1>
+
+    <p
+        className="
+      mt-3
+      max-w-md
+      text-base
+      leading-7
+      text-gray-500
+        "
+    >
+        Browse and manage all your recordings.
+    </p>
+
+</div>
 
     
 
 
- <div className="flex-1  min-h-0  overflow-y-auto space-y-2 border-y border-gray-300">
+<div className="
+    rounded-3xl
+    border
+    border-gray-200
+    shadow-md
+    bg-white
+    flex-1
+    min-h-0
+    mx-20
+  ">
+
+ <div className="
+      h-full
+      overflow-y-auto
+      custom-scrollbar
+
+      p-4
+      pr-2
+
+      space-y-3
+    ">
           {libraryRecordings.map((recording) => (
             <RecordingCard
               key={recording.id}
@@ -88,86 +140,27 @@ async function loadLibraryRecordings(page,limit) {
           ))}
         </div>
 
-        <div className="flex items-center justify-center gap-2 mt-6">
-
-  {/* Previous */}
-  <button
-    onClick={() => {
-      if (currentPage > 1) {
-        setPage(currentPage - 1);
-      }
+        </div>
+<div
+  className="
+    flex
+    justify-center
+    items-center
+    mt-4
+    mb-1
+  "
+>
+  <Pagination
+    count={totalPages}
+    page={page}
+    onChange={(event, page) => {
+      setPage(page);
     }}
-  >
-    ◀
-  </button>
-
-  {/* First Page */}
-  <button
-    onClick={() => setPage(1)}
-  >
-    1
-  </button>
-
-  {/* Left dots */}
-  {currentPage > 3 && (
-    <span>...</span>
-  )}
-
-  {/* Left Page */}
-  {currentPage > 2 && (
-    <button
-      onClick={() => setPage(leftPage)}
-    >
-      {leftPage}
-    </button>
-  )}
-
-  {/* Current Page */}
-  {currentPage !== 1 && currentPage !== totalPages && (
-    <button className="font-bold text-purple-700">
-      {currentPage}
-    </button>
-  )}
-
-  {/* Right Page */}
-  {currentPage < totalPages - 1 && (
-    <button
-      onClick={() => setPage(rightPage)}
-    >
-      {rightPage}
-    </button>
-  )}
-
-  {/* Right dots */}
-  {currentPage < totalPages - 2 && (
-    <span>...</span>
-  )}
-
-  {/* Last Page */}
-  {totalPages > 1 && (
-    <button
-      onClick={() => setPage(totalPages)}
-    >
-      {totalPages}
-    </button>
-  )}
-
-  {/* Next */}
-  <button
-    onClick={() => {
-      if (currentPage < totalPages) {
-        setPage(currentPage + 1);
-      }
-    }}
-  >
-    ▶
-  </button>
-
+  />
 </div>
-
-
     
-    </>)
+    </div>
+    )
 }
 
 export default RecordingLibrary
